c0b1f9147b929a654883eaecf7a168617f01a7a0
Kurt Ma <gladiator8072@gmail.com>
2023-03-16 14:52:52 +1300

    gradio

--- bin/DDGradio.py
+++ bin/DDGradio.py
@@ -11,35 +11,44 @@ sys.path.append("source")
 
 import DirectedDiffusion
 
-EX1 = [
-    "A painting of a tiger, on the wall in the living room",
-    "0.2,0.6,0.0,0.5",
-    "1,5",
-    5,
-    15,
-    1.0,
-    2094889,
+
+
+# prompt
+# boundingbox
+# prompt indices for region
+# number of trailing attention
+# number of DD steps
+# gaussian coefficient
+# seed
+EXAMPLES = [
+    [
+        "A painting of a tiger, on the wall in the living room",
+        "0.2,0.6,0.0,0.5",
+        "1,5",
+        5,
+        15,
+        1.0,
+        2094889,
+    ],
+    [
+        "a dog diving into a pool in sunny day",
+        "0.0,0.5,0.0,0.5",
+        "1,2",
+        10,
+        20,
+        5.0,
+        2483964026826,
+    ],
+    [
+        "A red cube above a blue sphere",
+        "0.4,0.7,0.0,0.5 0.4,0.7,0.5,1.0",
+        "2,3 6,7",
+        10,
+        20,
+        1.0,
+        1213698,
+    ],
 ]
-
-
-def fake_gan(a, b, c):
-    print(a, b, c)
-    images = [
-        (
-            random.choice(
-                [
-                    "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-                    "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
-                    "https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-                    "https://images.unsplash.com/photo-1546456073-92b9f0a8d413?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-                    "https://images.unsplash.com/photo-1601412436009-d964bd02edbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
-                ]
-            ),
-            f"label {i}" if i != 0 else "label" * 50,
-        )
-        for i in range(3)
-    ]
-    return images
 
 
 model_bundle = DirectedDiffusion.AttnEditorUtils.load_all_models(
@@ -78,14 +87,12 @@ def directed_diffusion(
         is_save_attn=False,
         is_save_recons=False,
     )
-    print(img.size)
     if is_draw_bbox and in_slider_ddsteps > 0:
         for r in roi:
             x0, y0, x1, y1 = [int(r_ * 512) for r_ in r]
-            print(x0, y0, x1, y1)
             image_editable = ImageDraw.Draw(img)
             image_editable.rectangle(
-                xy=[x0, y0, x1, y1], outline=(255, 0, 0, 255), width=5
+                xy=[x0, x1, y0, y1], outline=(255, 0, 0, 255), width=5
             )
 
     return img
@@ -109,8 +116,8 @@ def run_it(
     num_trailing_attn = [in_slider_trailings]
     if is_grid_search:
         num_affected_steps = [5, 10]
-        #noise_scale = [1.0, 1.5, 2.5]
-        #num_trailing_attn = [10, 20, 30, 40]
+        noise_scale = [1.0, 1.5, 2.5]
+        num_trailing_attn = [10, 20, 30, 40]
 
     param_list = [num_affected_steps, noise_scale, num_trailing_attn]
     param_list = list(itertools.product(*param_list))
@@ -149,6 +156,12 @@ def run_it(
 
 
 with gr.Blocks() as demo:
+    gr.Markdown(
+        """
+    # Directed Diffusion
+    Let's pin the object in the prompt as you wish!
+    """
+    )
     with gr.Row(variant="panel"):
         with gr.Column(variant="compact"):
             in_prompt = gr.Textbox(
@@ -167,7 +180,7 @@ with gr.Blocks() as demo:
                     placeholder="e.g., 0.1,0.5,0.3,0.6",
                 )
                 in_token_ids = gr.Textbox(
-                    label="Token idices",
+                    label="Token indices",
                     show_label=True,
                     max_lines=1,
                     placeholder="e.g., 1,2,3",
@@ -178,7 +191,7 @@ with gr.Blocks() as demo:
             with gr.Row(variant="compact"):
                 is_grid_search = gr.Checkbox(
                     value=False,
-                    label="Grid search? (Checked then sliders are ignored)",
+                    label="Grid search? (If checked then sliders are ignored)",
                 )
                 is_draw_bbox = gr.Checkbox(
                     value=True,
@@ -186,15 +199,22 @@ with gr.Blocks() as demo:
                 )
             with gr.Row(variant="compact"):
                 in_slider_trailings = gr.Slider(
-                    minimum=1, maximum=30, value=10, step=1, label="#trailings"
+                    minimum=0, maximum=30, value=10, step=1, label="#trailings"
                 )
                 in_slider_ddsteps = gr.Slider(
-                    minimum=0, maximum=20, value=10, step=1, label="#DDSteps"
+                    minimum=0, maximum=30, value=10, step=1, label="#DDSteps"
                 )
                 in_slider_gcoef = gr.Slider(
-                    minimum=1, maximum=5, value=1.0, step=0.1, label="GaussianCoef"
+                    minimum=0, maximum=10, value=1.0, step=0.1, label="GaussianCoef"
                 )
             btn = gr.Button("Generate image").style(full_width=False)
+
+            gr.Markdown(
+                """ Note:
+                1) Please click one of the examples below for quick setup.
+                2) if #DDsteps==0, it means the SD process runs without DD.
+                """
+            )
 
         gallery = gr.Gallery(
             label="Generated images", show_label=False, elem_id="gallery"
@@ -211,17 +231,11 @@ with gr.Blocks() as demo:
             is_draw_bbox,
             is_grid_search,
         ]
-
-        btn.click(
-            run_it,
-            inputs=args,
-            outputs=gallery,
-        )
-
+        btn.click(run_it, inputs=args, outputs=gallery)
     examples = gr.Examples(
-        examples=[EX1],
+        examples=EXAMPLES,
         inputs=args,
     )
 
 if __name__ == "__main__":
-    demo.queue().launch()
+    demo.queue().launch(share=True)
